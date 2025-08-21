6099c60644cacc06f38dabf0504bb0a4cc795a85
UmairJR <umairjr1265@gmail.com>
2025-08-21 09:07:59 +0200

    Changed data

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,12 +1,18 @@
 const degrees = [
   {
-    school: 'Terna Engineering College, Mumbai',
+    school: 'University of Paderborn, Paderborn, Germany',
+    degree: 'Master of Science in Computer Science',
+    link: 'https://www.uni-paderborn.de/en/university',
+    year: 2025,
+  },
+  {
+    school: 'Terna Engineering College, Mumbai, India',
     degree: 'Bachelor of Engineering in Computer Engineering',
     link: 'https://ternaengg.ac.in/',
     year: 2024,
   },
   {
-    school: 'Vivekananda Educational Society Polytechnic, Mumbai',
+    school: 'Vivekananda Educational Society Polytechnic, Mumbai, India',
     degree: ' Diploma in Computer Engineering',
     link: 'https://ves.ac.in/polytechnic/',
     year: 2021,
--- src/data/stats/personal.js
+++ src/data/stats/personal.js
@@ -64,7 +64,7 @@ const data = [
   {
     key: 'location',
     label: 'Current city',
-    value: 'Mumbai, IN',
+    value: 'Paderborn, DE',
   },
 ];
 
